4e0c37f8a1936689b02f378cb4161d615c4c56cf
carlos.aguilar_amk@outlook.com <carlos.aguilar_amk@outlook.com>
2023-12-15 10:02:55 -0600

    actualización

--- bodega.js
+++ bodega.js
@@ -1,44 +1,43 @@
 let inventario = [];
 
+// Cargar datos almacenados al cargar la página
+document.addEventListener("DOMContentLoaded", () => {
+  inventario = JSON.parse(localStorage.getItem("inventario")) || [];
+  actualizarInventario();
+});
+
 function agregarProducto() {
   const producto = document.getElementById("productoInput").value;
-  //   const proveedor = document.getElementById("proveedorInput").value;
-  //   const precio = document.getElementById("precioInput").value;
   const peso = document.getElementById("pesoInput").value;
   const bultos = parseInt(document.getElementById("bultosInput").value);
 
-  const productoExistente = inventario.find(
-    (p) => p.producto === producto && p.proveedor === proveedor
-  );
+  const productoExistente = inventario.find((p) => p.producto === producto);
 
   if (productoExistente) {
     productoExistente.bultos += bultos;
   } else {
-    inventario.push({ producto, proveedor, precio, peso, bultos });
+    inventario.push({ producto, peso, bultos });
   }
 
   limpiarInputs();
   actualizarInventario();
+  guardarDatos();
 }
 
 function quitarProducto() {
   const producto = document.getElementById("productoInput").value;
-  const proveedor = document.getElementById("proveedorInput").value;
 
-  const productoExistente = inventario.find(
-    (p) => p.producto === producto && p.proveedor === proveedor
-  );
+  const productoExistente = inventario.find((p) => p.producto === producto);
 
   if (productoExistente && productoExistente.bultos > 0) {
     productoExistente.bultos--;
     actualizarInventario();
+    guardarDatos();
   }
 }
 
 function limpiarInputs() {
   document.getElementById("productoInput").value = "";
-  document.getElementById("proveedorInput").value = "";
-  document.getElementById("precioInput").value = "";
   document.getElementById("pesoInput").value = "";
   document.getElementById("bultosInput").value = 1;
 }
@@ -51,11 +50,13 @@ function actualizarInventario() {
     const fila = document.createElement("tr");
     fila.innerHTML = `
       <td>${producto.producto}</td>
-      <td>${producto.proveedor}</td>
-      <td>${producto.precio}</td>
       <td>${producto.peso}</td>
       <td>${producto.bultos}</td>
     `;
     inventarioTableBody.appendChild(fila);
   });
+}
+
+function guardarDatos() {
+  localStorage.setItem("inventario", JSON.stringify(inventario));
 }
